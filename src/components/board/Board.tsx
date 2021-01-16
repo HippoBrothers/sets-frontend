9cea34ea0b08a7cd9cffdeb1841bd30e7343d917
Theo Depresle <theo.depresle@gmail.com>
2021-01-16 19:41:46 +0100

    Fixing responsive

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -12,21 +12,21 @@ type BoardProps = {
 
 const Board: React.FunctionComponent<BoardProps> = () => {
   const cards = useSelector((state: RootState) => state?.game?.cardsOnBoard);
+  const cardsCount = cards ? cards.length : 0;
+  const classGridSize = cardsCount - 12 > 0 ? "oversized" : "";
   return (
-    <div className="board">
-      {
-            (cards || []).map((c, i) => (
-              <Card
-                // eslint-disable-next-line react/no-array-index-key
-                key={i}
-                color={c.color}
-                number={c.number}
-                shape={c.shape}
-                fill={c.fill}
-                id={i}
-              />
-            ))
-        }
+    <div className={`board ${classGridSize}`}>
+      {(cards || []).map((c, i) => (
+        <Card
+          // eslint-disable-next-line react/no-array-index-key
+          key={i}
+          color={c.color}
+          number={c.number}
+          shape={c.shape}
+          fill={c.fill}
+          id={i}
+        />
+      ))}
     </div>
   );
 };
