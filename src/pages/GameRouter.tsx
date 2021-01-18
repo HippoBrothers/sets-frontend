5827a0a49426082768bb6bcaa5cdece7a6b89b49
Theo Depresle <theo.depresle@gmail.com>
2021-01-18 20:10:34 +0100

    Adding end game panel

--- src/pages/GameRouter.tsx
+++ src/pages/GameRouter.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import {RootState} from '../store/store';
-import GamePage from './game/GamePage';
-import LobbyPage from './lobby/LobbyPage';
-import EndPage from './end/EndPage';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import GamePage from "./game/GamePage";
+import LobbyPage from "./lobby/LobbyPage";
+import EndPage from "./end/EndPage";
+import { Redirect } from "react-router-dom";
 
 type GameRouterProps = {};
 
 const GameRouter: React.FunctionComponent<GameRouterProps> = () => {
-    const gameState = useSelector((state: RootState) => state.room.gameState);
-    if (gameState === 'waiting') {
-        return <LobbyPage/>;
-    }
-    if (gameState === 'playing' || gameState === 'buzzed') {
-        return <GamePage/>;
-    }
-    if (gameState === 'end') {
-        return <EndPage/>;
-    }
-    return <Redirect to='/' />;
+  const gameState = useSelector((state: RootState) => state.room.gameState);
+  if (gameState === "waiting") {
+    return <LobbyPage />;
+  }
+  if (gameState === "playing" || gameState === "buzzed") {
+    return <GamePage />;
+  }
+  if (gameState === "end") {
+    return <EndPage />;
+  }
+  return <Redirect to="/" />;
 };
 
 export default GameRouter;
--- src/pages/end/EndPage.tsx
+++ src/pages/end/EndPage.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Logo from "../../components/logo/Logo";
+import Rules from "../../components/Rules/Rules";
+import ScoreBoard from "../../components/scoreBoard/ScoreBoard";
 
 import { startVote } from '../../store/roomActions';
+import { getCurrentUser } from "../../store/slices/roomSlice";
+
+import "./endPage.scss";
 
 type EndPageProps = {
 
@@ -10,17 +17,34 @@ type EndPageProps = {
 
 const EndPage: React.FunctionComponent<EndPageProps> = () => {
   const dispatch = useDispatch();
+  const myProfile = useSelector(getCurrentUser);
+  
   return (
-    <div className="sets-page sets-page--end">
-      <h1>End</h1>
-      <Button
-        size="lg"
-        block
-        variant="warning"
-        onClick={() => dispatch(startVote())}
-      >
-        Start
-      </Button>
+    <div className="sets-page sets-page--end sets-page-common">
+      <Link to="/" className="logo-link">
+        <Logo />
+      </Link>
+      <div className="main-page-content">
+
+        <div className="winner">
+          <h1>GAME OVER</h1>
+          {/* <h3>WINNER : </h3> */}
+        </div>
+
+        <ScoreBoard />
+        <Button
+          size="lg"
+          block
+          variant={myProfile?.meta.vote ? "danger" : "warning"}
+          onClick={() => dispatch(startVote())}
+        >
+          Play again
+        </Button>
+
+        <hr />
+
+        <Rules />
+      </div>
     </div>
   );
 };
